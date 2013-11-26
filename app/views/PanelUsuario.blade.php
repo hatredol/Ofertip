356becbb45965385d789ded323940da4d028204f
Angelo Leva <angelokta7@gmail.com>
2013-11-26 11:39:18 -0500

    add PanelUsuario

---
+++ app/views/PanelUsuario.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel PHP Framework</title>
+</head>
+<body>
+    <div class="welcome">
+        <h1>You have arrived.</h1>
+    </div>
+</body>
+</html>
