fde781b4c7e22b9279750a2e8d5a232606b7e3f5
hatredol <daxatred@hotmail.com>
2013-11-26 19:18:39 -0500

    A

--- app/views/Usuario/formRegistro.blade.php
+++ app/views/Usuario/formRegistro.blade.php
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> e6520e11dfa152bf6715ab362dd0274e8ad19e4e
 <!-- Modal -->
 <div class="modal fade" id="modalRegistro" tabindex="-1" role="dialog" aria-labelledby="modalLabelRegistro" aria-hidden="true">
   <div class="modal-dialog">
