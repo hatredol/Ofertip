4909978554143fe647db563ff6f1acad4abffde4
hatredol <daxatred@hotmail.com>
2013-11-26 19:15:36 -0500

    Funcionalidad 40%

--- app/views/Usuario/formRegistro.blade.php
+++ app/views/Usuario/formRegistro.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 @extends('Template.templatePanelAdministracion')
 @section('Contenido')
 	<h1>Agregar Usuario</h1>
@@ -5,3 +6,39 @@
 	<h1>Agregar Usuario</h1>
 	<h1>Agregar Usuario</h1>
 @endsection
+=======
+<!-- Modal -->
+<div class="modal fade" id="modalRegistro" tabindex="-1" role="dialog" aria-labelledby="modalLabelRegistro" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="modalLabelRegistro">Registrate!</h4>
+      </div>
+      <div class="modal-body">
+        <form name="formAgregar" role="form">
+		  <div class="form-group">
+		    <label for="nombre">Nombre</label>
+		    <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Escribe un Nombre">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">Contraseña:</label>
+		    <input type="password" class="form-control" id="password" name="password" placeholder="Escribe una Contraseña">
+		  </div>
+		  <div class="form-group">
+		    <label for="recontrasena">Repetir-Contraseña:</label>
+		    <input type="password" class="form-control" id="repassword" placeholder="Vuelve a escribir la Contraseña">
+		  </div>
+		  
+		
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Salir</button>
+        <button type="submit" class="btn btn-default">Registrar</button>
+        </form>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+>>>>>>> e6520e11dfa152bf6715ab362dd0274e8ad19e4e
---
+++ app/views/WebSite/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Ofertip</title>
+			{{HTML::style('css/bootstrap.min.css')}}
+		    {{HTML::style('css/bootstrap-theme.min.css')}}
+		    
+	</head>
+	<body>
+		<header id="header" class="">
+		    <nav role="navigation">
+		      <div class="container">
+		        <!-- Brand and toggle get grouped for better mobile display -->
+		        <div class="navbar-header">
+		          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		            <span data-icon="" aria-hidden="true"></span>
+		          </button>
+		          <a class="navbar-brand" href=""><img src="img/logo.png" class="img-responsive" alt="" style="margin-bottom: 24px; width:50%"></a>
+		        </div>
+		        <div id="main-nav" class="collapse navbar-collapse navbar-collapse">          
+		          <ul class="nav navbar-nav fancy-nav navbar-right">
+		            <!--////////// NAVIGATION MENU //////////-->
+		            <li class="active"><a href="#section-home"><span data-hover="home">Inicio</span></a></li>
+		            <li><a href="#section-tour"><span data-hover="tour">¿Como Funciona?</span></a></li>
+		            <li><a href="#section-portfolio"><span data-hover="work">Beneficios</span></a></li>
+		            <li><a href="#section-pricing"><span data-hover="pricing">Planes</span></a></li>
+		            <li class=""><a href="#section-contact"><span data-hover="contact">Contacto</span></a></li>
+		            <li class=""><button class="btn btn-primary btn-md" data-toggle="modal" data-target="#modalRegistro">
+					  Registrarse
+					</button></li>
+		          </ul>
+		        </div><!-- /navbar-collapse -->
+		      </div><!-- /container -->
+		    </nav>
+		    @include('Usuario.formRegistro')
+	  	</header>
+		
+		@include('WebSite.funcionamiento')
+
+		<footer>
+
+		</footer>
+		{{HTML::script('http://codeorigin.jquery.com/jquery-2.0.3.min.js')}}
+		{{HTML::script('js/bootstrap.min.js')}}
+
+	</body>
+</html>
