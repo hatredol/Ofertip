c7f2e7697a4bf144584e59a47915cea4768f0e1c
hatredol <daxatred@hotmail.com>
2013-11-28 18:16:56 -0500

    Partes de Routes

--- app/routes.php
+++ app/routes.php
@@ -18,31 +18,14 @@ Route::get('/', function()
 
 
 /*PARTE PANEL ADMINISTRACION*/
-
-Route::get('Panel/Usuario', function()
-{
-	return View::make('Panel.Usuario.PanelAdministracionUsuario');
-});
 Route::get('/Panel', function()
 {
 	return View::make('formLogin');
 });
-Route::get('Panel/Perfil', function()
+Route::get('Panel/Usuario', function()
 {
-	return View::make('Panel.Usuario.PerfilUsuario');
+	return View::make('Panel.Usuario.PanelAdministracionUsuario');
 });
-
-/*Jorge*/
-
-/*Jorge*/
-
-Route::get('/Panel/dashboard', function()
-{
-	return View::make('Panel.Usuario.dashboard');
-});
-
-
-
 Route::get('Panel/Ofertas', function()
 {
 	return View::make('Panel.Usuario.OfertasTienda');
@@ -60,9 +43,25 @@ Route::get('Panel/Reportes', function()
 	return View::make('Panel.Usuario.ReportesTienda');
 });
 
+Route::get('Panel/Perfil', function()
+{
+	return View::make('Panel.Usuario.PerfilUsuario');
+});
+Route::get('/Panel/dashboard', function()
+{
+	return View::make('Panel.Usuario.dashboard');
+});
+
+
 /*Charlie*/
 Route::post('Panel/Usuario/AgregarBD',array('uses'=>'UsuarioController@AgregarBD'));
 Route::post('Panel/Usuario/Login',array('uses'=>'UsuarioController@Login'));
 
 
-//Tienda
+/*Yeto*/
+
+
+/*Jorge*/
+
+
+/*Angelo*/
